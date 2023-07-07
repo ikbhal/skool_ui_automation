0289e5e370fb3b325da891920c6ab77efc22d358
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-07 08:26:31 +0530

    add student automation

--- index.js
+++ index.js
@@ -8,18 +8,44 @@ const { remote } = require('webdriverio');
     }
   });
 
-  // Navigate to a website
-  await browser.url('http://localhost:3000/students');
+  try {
+    // Navigate to the add student form
+    await browser.url('http://localhost:3000/students/add');
 
-  // Find an element on the page
-  const element = await browser.$('h1');
+    // Generate random values for the form fields
+    const randomName = 'John Doe';
+    const randomMobileNumber = '1234567890';
+    const randomJoiningDate = '2023-07-07';
+    // Generate random values for other form fields as needed
 
-  // Get the text of the element
-  const text = await element.getText();
+    // Fill in the form fields with the generated random values
+    await (await browser.$('#name')).setValue(randomName);
+    await (await browser.$('#mobileNumber')).setValue(randomMobileNumber);
+    await (await browser.$('#joiningDate')).setValue(randomJoiningDate);
+    // Fill in other form fields as needed
 
-  // Print the text
-  console.log(text);
+    // Submit the form
+    await (await browser.$('button[type="submit"]')).click();
 
-  // Close the browser
-  await browser.deleteSession();
+    // Wait for the form submission to complete
+    await browser.waitUntil(async () => {
+      const currentUrl = await browser.getUrl();
+      return currentUrl === 'http://localhost:3000/students';
+    }, { timeout: 5000, timeoutMsg: 'Form submission failed' });
+
+    // Verify if the student was added to the student list page
+    const pageContent = await browser.getPageSource();
+    const studentListHtml = pageContent.toString();
+
+    if (studentListHtml.includes(randomName) && studentListHtml.includes(randomMobileNumber)) {
+      console.log('Student added successfully!');
+    } else {
+      console.log('Failed to add student.');
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  } finally {
+    // Close the browser
+    await browser.deleteSession();
+  }
 })();
